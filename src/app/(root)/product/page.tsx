e78542d4443368adb474e23f53e7677810d0ca83
Mahtab-217 <mahtabnasiri120089@gmail.com>
2026-07-18 10:42:44 +0430

    seachForm added

--- src/app/(root)/product/page.tsx
+++ src/app/(root)/product/page.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ProductCard from '../../../../components/ProductCard'
+
 import { myProducts } from '../../../../public/db/MyProduct'
+import SearchForm from '../../../../components/SearchForm'
 
 
 function productPage() {
 
   return (
     <div className='w-full bg-gray-300 h-screen flex justify-center items-center '>
-        <div className='w-full max-w-4xl flex p-5 justify-between items-center gap-2.5 backdrop-blur-md bg-white/80'>
+        <div className='w-full max-w-4xl  flex p-8 flex-col justify-between items-center gap-2.5 backdrop-blur-md bg-white/80'>
+         <SearchForm/>
+        <div className='flex justify-between flex-wrap'>
         {myProducts.map((x)=>(
             <ProductCard key={x.id} product={x}/>
         ))}
+        </div>
         </div>
     </div>
   )
