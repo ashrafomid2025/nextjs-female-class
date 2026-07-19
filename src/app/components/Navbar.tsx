71aaac856ebc58ec5c3782ee1346e41d0eb76ef1
MasoomaNaser <masoomanaseri770@gmail.com>
2026-07-19 11:30:01 +0430

    add navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="bg-blue-800 text-white p-4">
+      <h1 className=" text-xl font-bold ">Female Class</h1>
+      <ul>
+        <li>Home</li>
+        <li>Abuot</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
