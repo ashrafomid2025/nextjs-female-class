751dfc1053b5cbdebee01910cf781a569cc27ad6
MasoomaNaser <masoomanaseri770@gmail.com>
2026-07-20 10:38:56 +0430

    fix nabar links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 function Navbar() {
   return (
     <nav className="bg-blue-800 text-white p-4">
       <h1 className=" text-xl font-bold ">Female Class</h1>
       <ul>
-        <li>Home</li>
-        <li>Abuot</li>
-        <li>Contact</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
