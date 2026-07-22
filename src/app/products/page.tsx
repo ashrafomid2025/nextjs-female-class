713b4d5d7aaceeb5e54ccddf283bcc6583b56e25
ashrafomid2025 <omidmashraf@gmail.com>
2026-07-22 13:53:38 +0430

    conflicts resolved

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-function page() {
-=======
 function getRandomNumber(num: number) {
   return Math.floor(Math.random() * num);
 }
@@ -12,7 +8,6 @@ function page() {
     throw new Error("Something went wrong in products page");
   }
 
->>>>>>> ali
   return (
     <div>
       <h1>this is the product page</h1>
