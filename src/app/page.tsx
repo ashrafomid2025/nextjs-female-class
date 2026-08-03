ab4513c353e5a1c00e7aeecab68e6d5b620c31ed
Sitara Naseri <naserisitara@gmail.com>
2026-08-03 10:20:24 -0700

    changes in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center rounded-2xl border">
       <Badge>default badge</Badge>
       {/* navigating programmitically,
       
